a989884fb948b608a585f946d3a0d9deb9aa407d
bestdevelope <leabouillot7@gmail.com>
2024-07-27 14:41:20 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP-projet-1</title>
+</head>
+
+<body>
+
+    <h1 class="hero">
+
+    </h1>
+    <div class="container">
+        <div class="holder">
+            <form action="ondex.php" method="POST">
+                <div class="wrapper">
+                    <input type="text" placeholder="Enter Username" name="name">
+                </div>
+                <br>
+                <br>
+                <div class="wrapper">
+                    <input type="submit" value="submit" name="submit">
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <h4 class="footer">Made with <span>💖</span>by Léa B.</h4>
+</body>
+
+</html>
