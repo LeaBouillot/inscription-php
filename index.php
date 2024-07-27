3fd40e765acbb5288b4556b8a4a263ef2f5292c4
bestdevelope <leabouillot7@gmail.com>
2024-07-27 15:48:53 +0200

    css

--- index.php
+++ index.php
@@ -1,25 +1,80 @@
-<?php
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP-projet-1</title>
+
+    <title>User-Login-PHP</title>
 </head>
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: sans-serif;
+    }
+
+    .container {
+        height: 100vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    h1 {
+        font-weight: 600;
+        color: #a363aa;
+        font-size: 40px;
+
+    }
+
+    .holder {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .hero {
+        left: 30%;
+    }
+
+    .holder input[type="text"] {
+        height: 30px;
+        background: transparent;
+        border: none;
+        outline: none;
+        font-size: 20px;
+        border-bottom: 2px solid #a363aa;
+    }
+
+    .holder input[type="submit"] {
+        display: flex;
+        width: 200px;
+        align-items: center;
+        padding: 15px 30px;
+        font-size: 16px;
+        color: #fff;
+        background: linear-gradient(270deg, #ff994f, #fa6d86);
+        border: none;
+        border-radius: 50px;
+    }
+</style>
 
 <body>
-
-    <h1 class="hero">
-
-    </h1>
+    <?php
+    if (isset($_POST['submit'])) {
+        echo "Nous somme heureux de vous retouver";
+    } else {
+        echo "Veuillez entrer votre nom";
+    }
+    ?>
     <div class="container">
+
         <div class="holder">
-            <form action="ondex.php" method="POST">
+            <h1 class="hero"><?php echo "The user's name is " . $_POST['name'] . " 💖"; ?>
+            </h1>
+            <form action="index.php" method="POST">
                 <div class="wrapper">
+                    <br>
                     <input type="text" placeholder="Enter Username" name="name">
                 </div>
                 <br>
@@ -28,10 +83,11 @@
                     <input type="submit" value="submit" name="submit">
                 </div>
             </form>
+
         </div>
     </div>
 
-    <h4 class="footer">Made with <span>💖</span>by Léa B.</h4>
+
 </body>
 
 </html>
